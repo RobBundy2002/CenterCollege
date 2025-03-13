ca3bd2015544c2780caee27fa33f2117d74b796f
Rob Bundy <robbielbundy@Gmail.com>
2025-03-13 00:19:58 -0400

    Scroll to top fix

--- center-college/src/Miscellaneous/ScrollToTop.js
+++ center-college/src/Miscellaneous/ScrollToTop.js
@@ -5,7 +5,7 @@ const ScrollToTop = () => {
     const location = useLocation();
 
     useEffect(() => {
-        window.scrollTo(0, 5);
+        window.scrollTo(0, 0);
     }, [location]);
 
     return null;
